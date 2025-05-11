a5cfb79fd2fd5a313625d65b192d65b3d296adfb
shahid285 <mohammedshahid.r@gmail.com>
2025-05-11 18:32:47 +0530

    Configured Spring Cloud Config

--- src/main/java/com/example/ordermanagement/OrderManagementApplication.java
+++ src/main/java/com/example/ordermanagement/OrderManagementApplication.java
@@ -3,7 +3,7 @@ package com.example.ordermanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableConfigServer
+
 @SpringBootApplication
 public class OrderManagementApplication {
 
